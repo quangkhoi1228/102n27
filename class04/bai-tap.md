4bfad4e43b83488983dd946b9eafca494d4add3c
khoitq-acl <khoitq@americancodelab.com>
2024-05-03 23:04:45 +0700

    Add class 05

---
+++ class04/bai-tap.md
@@ -0,0 +1,5 @@
+# Bài tập class 05
+
+## Vẽ wire frame project trên Miro
+
+## Code HTML, CSS cho phần header và danh sách sản phẩm chính (danh sách món ăn, danh sách kèo betting),..
---
+++ class04/main.js
@@ -0,0 +1,53 @@
+// const ageString = prompt("Bạn bao nhiêu tuổi?");
+// // string - chữ "20"
+
+// console.log("ageString: ", ageString)
+
+// const ageNumber = Number(ageString);
+
+// console.log("ageNumber: ", ageNumber)
+
+// if (ageNumber > 18) {
+//   alert("Bạn đã trưởng thành");
+// } else {
+//   alert("Bạn chưa trưởng thành");
+// }
+
+// variable
+var myName = "Khôi";
+
+// Number - int, float, double,... 
+var myAge = 2.5;
+
+// Boolean - true, false 
+var hasChildren = false;
+
+// Array - []
+var numbers = [1, 2, 3, 4, 5];
+
+// Object ~ Class
+var khoi = {
+  // key: $value,
+  name: "Khôi",
+  age: 27,
+  hasChildren: true
+}
+
+
+var duy = {
+  // key: $value,
+  name: "Duy",
+  age: 21,
+  hasChildren: false
+}
+
+
+console.log(duy.name)
+
+var sum = 10 + 5; 
+
+// sum = 15
+
+// alert(sum)
+
+
